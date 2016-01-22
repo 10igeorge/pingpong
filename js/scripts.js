4fe756fbd28251b9387f1704a0837628b8ed3cb2
izzy <student@epicodus.com>
2016-01-22 15:17:02 -0800

    add spec functionality for replacing numbers divisible by 15 with 'pingpong' and reordered conditional statement

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@ var pingPong = function(userNumber){
   for (var i = 1; i <= userNumber; i++){
     while (i <= userNumber){
       array.push(i);
-        if (i % 3 === 0){
-          array.splice([i-1], 1, "<li>ping</li>");
+        if (i % 15 === 0){
+          array.splice([i-1], 1, "<li>pingpong</li>");
           i += 1;
         } else if (i % 5 === 0){
           array.splice([i-1], 1, "<li>pong</li>");
+          i += 1;
+        } else if (i % 3 === 0){
+          array.splice([i-1], 1, "<li>ping</li>");
           i += 1;
         } else {
           i += 1;
--- specs/specs.js
+++ specs/specs.js
@@ -4,6 +4,9 @@
 //   });
 // });
 describe('pingPong', function() {
+  it("replace any number divisible by 15 with 'pingpong' and add into an unordered list", function() {
+    expect(pingPong(15)).to.equal("1, 2, <li>ping</li>, 4, <li>pong</li>, <li>ping</li>, 7, 8, <li>ping</li>, <li>pong</li>, 11, <li>ping</li>, 13, 14, <li>pingpong</li>");
+  });
   it("replace any number divisible by 3 with 'ping' and add into an unordered list", function() {
     expect(pingPong(3)).to.equal("1, 2, <li>ping</li>");
   });
