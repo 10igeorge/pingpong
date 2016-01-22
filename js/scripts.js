5472ee4475257e2e95f3cc8b2cd7026ee6ddbbc0
izzy <student@epicodus.com>
2016-01-22 14:45:17 -0800

    add spec functionality for replacing numbers divisible by 3 with 'ping'

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-var countingUp = function(userNumber){
-  var output = [];
-  if (typeof userNumber === 'number'){
-    for (var i = 1; i <= userNumber; i++) {
-        output.push(i);
-    } return output;
-  } else {
-    return "Error";
-  }
+var pingPong = function(userNumber){
+  var array = [];
+  for (var i = 1; i <= userNumber; i++){
+    if (i % 3 === 0){
+      array.push("<li>ping</li>");
+
+    }
+  } return array.join(" ");
 };
+
+
+
+
+//
+// var countingUp = function(userNumber){
+//   debugger;
+//   var output = [];
+//   if (typeof userNumber === 'number'){
+//     for (var i = 1; i <= userNumber; i++) {
+//         output.push(i);
+//     } return output;
+//   }
+// };
+//
+//
+// countingUp(10);
--- specs/specs.js
+++ specs/specs.js
@@ -1,10 +1,13 @@
-describe('countingUp', function() {
-  it("will count up to the user number by 1", function() {
-    expect(countingUp(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
-  });
-});
+// describe('countingUp', function() {
+//   it("will count up to the user number by 1", function() {
+//     expect(countingUp(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+//   });
+// });
 describe('pingPong', function() {
-  it("replace any number divisible by 15 with 'pingpong'", function() {
-    expect(pingPong(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+  it("replace any number divisible by 3 with 'ping' and add into an unordered list", function() {
+    expect(pingPong(3)).to.equal("<li>ping</li>");
+  });
+  it("replace any number divisible by 5 with 'pong' and add into an unordered list", function() {
+    expect(pingPong(5)).to.equal("<li>pong</li>");
   });
 });
